18ece4abde7c776bbbc2fbca747b74a1bb86332b
vincecfl <vincecfl@gmail.com>
2020-09-30 18:25:08 +0800

    add

--- lib/tron_web.dart
+++ lib/tron_web.dart
@@ -10,6 +10,8 @@ class TronWeb extends StatefulWidget {
 
 class _TronWebState extends State<TronWeb> {
   bool tronFlag = false;
+  String account = '';
+
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +20,8 @@ class _TronWebState extends State<TronWeb> {
         children: <Widget>[
           SizedBox(height: 10),
           _tronWidget(context),
+          SizedBox(height: 10),
+          _accountWidget(context),
         ],
       ),
     );
@@ -44,6 +48,39 @@ class _TronWebState extends State<TronWeb> {
             SizedBox(width: 10),
             Text(
               tronFlag ? 'yes' : 'no',
+              style: GoogleFonts.lato(
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _accountWidget(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // tronWeb.defaultAddress.base58
+        var result = js.context["tronWeb"]["defaultAddress"]["base58"];
+        js.context.callMethod('alert', [result]);
+        setState(() {
+          account = result.toString();
+        });
+      },
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'account: ',
+              style: GoogleFonts.lato(
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(width: 10),
+            Text(
+              '$account',
               style: GoogleFonts.lato(
                 fontSize: 30,
               ),
